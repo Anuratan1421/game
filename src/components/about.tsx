4fad6f307e243c11ff547ae1111537820251802e
Anuratan1421 <anuratanbahadure@gmail.com>
2025-02-23 15:55:37 +0530

    helll

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -49,7 +49,7 @@ export const About = () => {
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
-            src="/img/GANDHARVA (2).png"
+            src="/img/a.jpg"
             alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
